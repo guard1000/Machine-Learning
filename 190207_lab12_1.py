0fe8a37fc475505ef1de8d4d9da96ecfc476702e
guard1000 <cjsdnr885@naver.com>
2019-02-08 03:41:30 +0900

    190207_lab12_1.py

---
+++ 190207_lab12_1.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.contrib import rnn
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+sess = tf.InteractiveSession()
+
+# One hot encoding for each char in 'hello'
+h = [1, 0, 0, 0]
+e = [0, 1, 0, 0]
+l = [0, 0, 1, 0]
+o = [0, 0, 0, 1]
+
+'''
+with tf.variable_scope('3_batches_dynamic_length') as scope:
+    # One cell RNN input_dim (4) -> output_dim (5). sequence: 5, batch 3
+    # 3 batches 'hello', 'eolll', 'lleel'
+    x_data = np.array([[h, e, l, l, o],
+                       [e, o, l, l, l],
+                       [l, l, e, e, l]], dtype=np.float32)
+    pp.pprint(x_data)
+
+    hidden_size = 2
+    cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size, state_is_tuple=True)
+    outputs, _states = tf.nn.dynamic_rnn(
+        cell, x_data, sequence_length=[5, 3, 4], dtype=tf.float32)
+    sess.run(tf.global_variables_initializer())
+    pp.pprint(outputs.eval())
+'''
+with tf.variable_scope('initial_state') as scope:
+    batch_size = 3
+    x_data = np.array([[h, e, l, l, o],
+                       [e, o, l, l, l],
+                       [l, l, e, e, l]], dtype=np.float32)
+    pp.pprint(x_data)
+
+    # One cell RNN input_dim (4) -> output_dim (5). sequence: 5, batch: 3
+    hidden_size = 2
+    cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size, state_is_tuple=True)
+    initial_state = cell.zero_state(batch_size, tf.float32)
+    outputs, _states = tf.nn.dynamic_rnn(cell, x_data,
+                                         initial_state=initial_state, dtype=tf.float32)
+    sess.run(tf.global_variables_initializer())
+    pp.pprint(outputs.eval())
